95187132cbd46b0c03add72122002b5e63e1cd2c
Codeal <145662064+juil1-kim@users.noreply.github.com>
2024-11-29 16:05:46 +0900

    Revert "administrators, branches 패키지 업데이트"

--- src/main/java/org/example/logistics/branches/BranchesVO.java
+++ src/main/java/org/example/logistics/branches/BranchesVO.java
@@ -1,11 +1,14 @@
 package org.example.logistics.branches;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 public class BranchesVO {
-    private int branchId;
-    private String name;
-    private String location;
+    private int branchID;
+    private String branchName;
+    private String branchLocate;
 }
-
